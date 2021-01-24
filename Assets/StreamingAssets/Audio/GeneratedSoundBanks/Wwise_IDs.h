7c467e87c2aa58af36120da4eb172024fe1187f0
Soundlancer <77804867+Soundlancer@users.noreply.github.com>
2021-01-25 00:03:28 +0300

    Test

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,69 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMB_01 = 2199192635U;
+        static const AkUniqueID AMB_02 = 2199192632U;
+        static const AkUniqueID AMB_04 = 2199192638U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID GARBAGE = 2092055118U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID LAMP = 657744959U;
+        static const AkUniqueID LAMP2 = 4135178015U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID RAIN = 2043403999U;
+        static const AkUniqueID TALKING = 3375731105U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_OCCLUSION = 1664992182U;
+        static const AkUniqueID RTPC_OCCLUSION2 = 4030404528U;
+        static const AkUniqueID RTPC_OCCLUSION_AMB1 = 1653615052U;
+        static const AkUniqueID RTPC_OCCLUSION_AMB4 = 1653615049U;
+        static const AkUniqueID RTPC_OCCLUSION_LAMP2 = 2469836583U;
+        static const AkUniqueID RTPC_OCCLUSION_MUSIC = 1631142046U;
+        static const AkUniqueID RTPC_OCCLUSION_RADIO = 982894454U;
+        static const AkUniqueID RTPC_OCCLUSION_RAIN = 3066961559U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MASTER = 4056684167U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENT = 77978275U;
+        static const AkUniqueID AMBISONIC_BUS = 2269705971U;
+        static const AkUniqueID CHARACTER = 436743010U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
---
+++ Assets/scripts/footsteps_script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footsteps_script : MonoBehaviour
+{
+    public AK.Wwise.Event footstepsEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        { footstepsEvent.Post(gameObject); }
+        if (Input.GetKeyUp(KeyCode.W))
+        { footstepsEvent.Stop(gameObject); }
+        if (Input.GetKeyDown(KeyCode.A))
+        { footstepsEvent.Post(gameObject); }
+        if (Input.GetKeyUp(KeyCode.A))
+        { footstepsEvent.Stop(gameObject); }
+        if (Input.GetKeyDown(KeyCode.S))
+        { footstepsEvent.Post(gameObject); }
+        if (Input.GetKeyUp(KeyCode.S))
+        { footstepsEvent.Stop(gameObject); }
+        if (Input.GetKeyDown(KeyCode.D))
+        { footstepsEvent.Post(gameObject); }
+        if (Input.GetKeyUp(KeyCode.D))
+        { footstepsEvent.Stop(gameObject); }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        { footstepsEvent.Post(gameObject); }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        { footstepsEvent.Stop(gameObject); }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        { footstepsEvent.Post(gameObject); }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        { footstepsEvent.Stop(gameObject); }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        { footstepsEvent.Post(gameObject); }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        { footstepsEvent.Stop(gameObject); }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        { footstepsEvent.Post(gameObject); }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        { footstepsEvent.Stop(gameObject); }
+    }
+}
